d660627f9946c715ed9465c5545c94ca277d64d4
Shelly Huang <shellyhuang81@gmail.com>
2018-10-04 08:28:37 -0400

    added upload feature, added folder where images are saved

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
-
+from flask_uploads import UploadSet, IMAGES, configure_uploads
 app = Flask(__name__)
 
-@app.route('/')
+
+photos = UploadSet('photos', IMAGES) #type of file to be uploaded <- IMAGES
+app.config['UPLOAD_PHOTOS_DEST'] = 'static/imgs'
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+	# if request.method == 'POST' and 'img' in request.files:
+	# 	filename = photos.save(request.files[img])
+	# 	return filename
 	return render_template('index.html')
 
 if __name__ == '__main__':
