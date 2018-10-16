ebfbb90ab976064d61d98cc9c733bd1f5aef8ca2
Qiuqun Wang <qiuqunwang12@gmail.com>
2018-10-16 19:13:22 -0400

    added more filter and render picture upon upload and remove submit button

--- app/routes.py
+++ app/routes.py
@@ -36,6 +36,14 @@ def index():
 			image = image.filter(ImageFilter.GaussianBlur(20))
 		elif(filter == "f3"):
 			image = image.filter(ImageFilter.CONTOUR)
+		elif(filter == "f4"):
+			image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+		elif(filter == "f5"):
+			image = image.filter(ImageFilter.EMBOSS)
+		elif(filter == "f6"):
+			image = image.filter(ImageFilter.FIND_EDGES)
+
+		#SMOOTH_MORE
 
 
 		ts = time.time()
